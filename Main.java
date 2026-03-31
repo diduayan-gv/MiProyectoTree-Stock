c5b1a7c8abfdbf2e2ca9e8242b82d20cfaf90e8e
Diduayan Gomez <diduayan@gmail.com>
2026-03-31 18:07:12 -0500

    Se crea logica en Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArbolInventario inventario = new ArbolInventario();
+        int opcion;
+
+        do {
+            System.out.println("\n--- SISTEMA TREE-STOCK ---");
+            System.out.println("1. Registrar Producto");
+            System.out.println("2. Mostrar Inventario (Ordenado)");
+            System.out.println("3. Buscar Producto");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine(); // Limpiar buffer
+                    System.out.print("Ingrese Nombre: ");
+                    String nombre = sc.nextLine();
+                    inventario.insertar(id, nombre);
+                    System.out.println("Producto registrado.");
+                    break;
+                case 2:
+                    System.out.println("\n--- LISTADO DE PRODUCTOS ---");
+                    inventario.mostrarInorden(inventario.raiz);
+                    break;
+                case 3:
+                    System.out.print("ID a buscar: ");
+                    int idBuscar = sc.nextInt();
+                    Producto hallado = inventario.buscar(inventario.raiz, idBuscar);
+                    if (hallado != null) {
+                        System.out.println("Existe: " + hallado.nombre);
+                    } else {
+                        System.out.println("Producto no encontrado.");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+            }
+        } while (opcion != 0);
+    }
 }
